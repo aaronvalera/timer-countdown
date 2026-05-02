178ee58880661d52d85c0d95a348fc43782c0e10
Aaron Valera <aaronrvalera@gmail.com>
2026-05-02 15:17:38 -0400

    Refactor script

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 // Selectors
+const video = document.querySelector("#rocket-launch-video");
+const elementsContainer = document.querySelector("#countdown-timer");;
+const btnsContainer = document.querySelector("#time-buttons");
 const timeBtns = document.querySelectorAll("button");
-const countDownDisplay = document.querySelector("#countdown-display");
-const title = document.querySelector("#title")
+const title = document.querySelector("#title");
+const countdownDisplay = document.querySelector("#countdown-display");
 
+// Variables
 let index = 0;
+let countDown = undefined;
 
+// Timer function
 const timer = () => {
-    countDownDisplay.textContent = index;
-    console.log(index);
+    countdownDisplay.textContent = index;
     index--;
     if(index < 0) {
         clearInterval(countDown);
-        console.log("Interval cleared.");
+        video.play();
+        title.textContent = "LAUNCH!";
         setTimeout(() => {
-            countDownDisplay.textContent = "";
-            title.textContent = "Launch!";
-        }, 2000);
-    }
-}
+            elementsContainer.remove();
+        }, 1000);
+    };
+};
 
-let countDown = undefined;
-
-timeBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        index = parseInt(btn.value);
-        title.textContent = "";
-        clearInterval(countDown)
+// Event listener
+timeBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        index = parseInt(button.value);
+        btnsContainer.innerHTML = "";
+        title.textContent = "Initiating launch in...";
         countDown = setInterval(timer, 1000);
     });
 });
